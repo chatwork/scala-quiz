73ff6bfd09b5bef53ce6923087701e40215e6b46
watarui <wataruby@gmail.com>
2020-09-28 01:42:31 +0900

    Q2 BTree テスト通った

--- src/main/scala/com/chatwork/quiz/misc/BTree.scala
+++ src/main/scala/com/chatwork/quiz/misc/BTree.scala
@@ -48,7 +48,7 @@ sealed trait Node {
 /**
   * 枝を表す[[Node]]。
   *
-  * @param left　左の[[Node]]
+  * @param left  左の[[Node]]
   * @param value 値
   * @param right 右の[[Node]]
   */
@@ -56,36 +56,49 @@ case class Branch(left: Node, value: Int, right: Node) extends Node {
 
   val size: Int = left.size + right.size + 1
 
-  val sum: Int = ???
+  val sum: Int = left.sum + right.sum + value
 
-  val avg: Double = ???
+  val avg: Double = sum / size
 
-  val max: Int = ???
+  val max: Int = List(left.max, right.max, value).max
 
-  val min: Int = ???
+  val min: Int = List(left.max, right.max, value).min
 
-  def find(value: Int): Option[Node] = ???
+  def find(value: Int): Option[Node] = {
+
+    lazy val l = left find value
+    lazy val r = right find value
+
+    if (value == this.value) Some(this) else {
+      l match {
+        case Some(bt) => Some(bt)
+        case _ => r match {
+          case Some(bt) => Some(bt)
+          case _ => None
+        }
+      }
+    }
+  }
 
 }
 
 /**
   * 葉を表す[[Node]]。
   *
-  * @param value 値
   */
 case class Leaf(value: Int) extends Node {
 
   val size: Int = 1
 
-  val sum: Int = ???
+  val sum: Int = value
 
-  val avg: Double = ???
+  val avg: Double = value
 
-  val max: Int = ???
+  val max: Int = value
 
-  val min: Int = ???
+  val min: Int = value
 
-  def find(value: Int): Option[Node] = ???
+  def find(value: Int): Option[Node] = if (value == this.value) Some(this) else None
 
 }
 
@@ -106,7 +119,7 @@ case class BTree(node: Node) {
 
   lazy val min: Int = node.min
 
-  def find(value: Int): Option[Node] = node.find(value)
+  def find(value: Int): Option[Node] = node find value
 
 }
 
@@ -121,6 +134,11 @@ object BTree {
     * @param values ノードに格納する値の集合
     * @return [[BTree]]
     */
-  def apply(values: List[Int]): BTree = ???
+  def apply(values: List[Int]): BTree = {
+    if (values.length == 1) BTree(Leaf(values.head)) else {
+      val c = values.length / 2
+      BTree(Branch(apply(values.take(c)).node, values(c), apply(values.takeRight(c)).node))
+    }
+  }
 
 }
